cbb989dd67260074697762d0042bedd42201f9b5
Grapezzz <rnichol111@outlook.com>
2017-09-25 14:29:24 +1300

    Test Scripts and Scene

---
+++ Assets/Testing/CollisionTesting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTesting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        IntegrationTest.Pass();
+    }
+}
---
+++ Assets/Testing/TestMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMovement : MonoBehaviour {
+
+    public Vector2 startPos;
+    public GameObject objects;
+
+	// Use this for initialization
+	void Start () {
+
+        startPos = this.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (startPos.y != this.transform.position.y)
+            IntegrationTest.Pass(gameObject);
+
+	}
+}
